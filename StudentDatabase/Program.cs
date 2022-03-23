3a278c42dd1e17177b54247a7be5f437174c7c31
William_Twomey <irowt@mail.com>
2022-03-23 00:17:45 +0000

    Code first student database that adds one student to the database

---
+++ StudentDatabase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Sarah" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.WriteLine(stud.StudentName + " added to Database");
+                Console.ReadKey();
+                Console.Beep();
+            }
+           
+        }
+    }
+}
